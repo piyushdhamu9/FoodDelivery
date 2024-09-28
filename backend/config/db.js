ca895d9798f738a0be4d86a09af8db327b93efc0
piyushdhamu9 <piyushdhamu9@gmail.com>
2024-09-29 04:58:42 +0530

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://piyushdhamu9:014560@cluster0.d0szi.mongodb.net/MERN"
+      ""
     )
     .then(() => console.log("Database connected"));
 };
